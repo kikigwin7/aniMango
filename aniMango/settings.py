7dff2f4c3bd2ae0f8247ce95bebc472afa2b57a9
Jai Sharma <jai.p.sharma1@gmail.com>
2016-09-19 01:13:01 +0100

    Added placeholder homepage content

--- aniMango/settings.py
+++ aniMango/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# App static sub directory
+# App specific static file sub directory
 STATIC_URL = '/static/'
 
+# Staic files served in testing
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Disable https during testing
+SECURE_SSL_REDIRECT = False
