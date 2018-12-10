73d803e45251efd1d712f37dfa60b1186e5b577f
Alfie Powers <powersalfie@gmail.com>
2018-12-10 20:37:23 +0000

    101218v1
    Added updater to library content

--- library/management/commands/updateFullSeries.py
+++ library/management/commands/updateFullSeries.py
@@ -1,6 +1,29 @@
-import library.models
+from django.core.management.base import BaseCommand
+from library.models import Series
+from anilist_api.anilist import api_get_info
+import time
 
-def updateValues():
-    series_objects  = library.Series.objects.filter()
-    for series_object in series_objects:
-        print(series_object.api_id)
+def updateImage(item):
+    data = api_get_info(item)
+    print('Updated the picture of ' + item.title + ' to ' + data['coverImage']['large'])
+    item.cover_link = data['coverImage']['large']
+    item.save()
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def add_arguments(self, parser):
+        parser.add_argument('field', nargs='+', type=str)
+
+    def handle(self, *args, **kwargs):
+
+        print("Checking all of the series against the current values")
+
+        library = Series.objects.all()
+        for item in library:
+            time.sleep(1)
+            if kwargs["field"][0] == "image":
+                updateImage(item)
+            else:
+                break;
+
